3432618aab3fb53aae89ff0399f55203f66e08cc
Haruto Hamano <haruto.yhs318@gmail.com>
2024-02-22 16:11:18 +0900

    Initial Commit

---
+++ Friendy/FriendyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendyApp.swift
+//  Friendy
+//
+//  Created by 濱野遥斗 on 2024/02/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
