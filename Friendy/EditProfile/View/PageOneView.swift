1ddec26e729e799f2b64904f85581cc158e1e72e
Haruto Hamano <haruto.yhs318@gmail.com>
2024-02-25 16:58:43 +0900

    created first page view of onBoarding for edit profile

--- Friendy/EditProfile/View/PageOneView.swift
+++ Friendy/EditProfile/View/PageOneView.swift
@@ -7,12 +7,45 @@
 
 import SwiftUI
 
+
 struct PageOneView: View {
+    @Binding var inputName: String
+    @Binding var inputDestination: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10){
+            VStack(spacing: 20){
+                Text("名前と表示名を入力してください").font(.custom("NotoSansJP-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333"))).padding(.horizontal)
+                Spacer()
+                
+                VStack(spacing: 10){
+                    HStack{
+                        Text("名前").font(.custom("NotoSansJP-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
+                        Spacer()
+                    }
+                    CustomTextField(category: "名前を入力してください")
+                }.padding(.horizontal)
+                Spacer()
+                
+                VStack(spacing: 10){
+                    HStack{
+                        Text("表示名").font(.custom("NotoSansJP-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
+                        Spacer()
+                    }
+                    CustomTextField(category: "ニックネームを入力してください")
+                }.padding(.horizontal)
+                Spacer()
+                
+                Spacer()
+            }.padding(.horizontal)
+        }
     }
 }
 
-#Preview {
-    PageOneView()
+
+struct PageOneView_Previews: PreviewProvider {
+    static var previews: some View {
+//        OnBoarding()
+        PageOneView(inputName: .constant("京都旅行"), inputDestination:.constant("清水寺"))
+    }
 }
