f8d9d2e524247ed015b8921d5d3f5575f5dd41c7
Haruto Hamano <haruto.yhs318@gmail.com>
2024-02-25 16:57:38 +0900

    create customized textfield for edit view

--- Friendy/EditProfile/components/CustomTextField.swift
+++ Friendy/EditProfile/components/CustomTextField.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct CustomTextField: View {
+    var category: String
+    @State var inputInfo: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField(category, text: $inputInfo)
+            .padding()
+//            .padding(.horizontal)
+            .foregroundColor(Color(UIColor(hexString: "333333")))
+            .font(.custom("NotoSansJP-Regular", size: 14))
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color(UIColor(hexString: "C9C9C9")), lineWidth: 1)
+                .foregroundColor(.clear)
+                .frame(height: 40)
+            )
     }
 }
 
 #Preview {
-    CustomTextField()
+    CustomTextField(category: "名前を入力してください")
 }
