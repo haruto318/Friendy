374901887ed885d5c31d8fd90d09ce61589f0b8b
Haruto Hamano <haruto.yhs318@gmail.com>
2024-02-25 16:58:55 +0900

    created second page view of onBoarding for edit profile

--- Friendy/EditProfile/View/PageTwoView.swift
+++ Friendy/EditProfile/View/PageTwoView.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct PageTwoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20){
+            VStack(spacing: 20){
+                Text("プロフィールに表示する情報を入力してください").font(.custom("NotoSansJP-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333"))).padding(.horizontal)
+                Spacer()
+                
+                VStack(spacing: 10){
+                    HStack{
+                        Text("連絡用メールアドレス").font(.custom("NotoSansJP-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
+                        Spacer()
+                    }
+                    CustomTextField(category: "メールアドレスを入力してください")
+                }.padding(.horizontal)
+                    .padding(.bottom, 20)
+                
+                Divider()
+                
+                VStack(spacing: 20){
+                    HStack{
+                        Text("SNSアカウント").font(.custom("NotoSansJP-Regular", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
+                        Spacer()
+                    }
+                    CustomTextField(category: "X(旧Twitter)")
+                    CustomTextField(category: "Instagram")
+                    CustomTextField(category: "GitHub")
+                    CustomTextField(category: "その他 (Qiita/Zenn/Noteなど)")
+                }.padding(.horizontal)
+                    .padding(.top, 10)
+                Spacer()
+                
+                Spacer()
+            }.padding(.horizontal)
+        }
     }
 }
 
