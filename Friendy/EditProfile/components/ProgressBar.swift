49c358a2ccde13686fb7a55a6dd91415b581812e
Haruto Hamano <haruto.yhs318@gmail.com>
2024-02-25 16:57:11 +0900

    create progress bar for edit view

--- Friendy/EditProfile/components/ProgressBar.swift
+++ Friendy/EditProfile/components/ProgressBar.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct ProgressBar: View {
+    @Binding var progressVal: Double
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){ ///画面上部のバー
+            Text("プロフィールを編集").padding().font(.custom("NotoSansJP-Medium", size: 20.0)).foregroundStyle(Color(UIColor(hexString: "333333")))
+            ProgressView(value: min(max(progressVal, 0.0), 100), total: 100)
+                .animation(.easeInOut, value: progressVal)//バーのアニメーション
+                .tint(Color(UIColor(hexString: "333333")))//バーの色
+                .scaleEffect(x: 1, y: 0.5)//バーの高さ
+        }
     }
 }
 
-#Preview {
-    ProgressBar()
-}
+//#Preview {
+//    ProgressBar()
+//}
