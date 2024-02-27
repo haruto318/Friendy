f39e2444771f099620893fa89b61461f2040348e
Haruto Hamano <haruto.yhs318@gmail.com>
2024-02-27 17:18:19 +0900

    fixed updating system of database in DBEditProfileModel

--- Friendy/Models/DBEditProfileModel.swift
+++ Friendy/Models/DBEditProfileModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RealmSwift
+import SwiftUI
 
 class DBEditProfileModel: ObservableObject{
     @Published var image: Data = Data()
@@ -29,7 +30,10 @@ class DBEditProfileModel: ObservableObject{
     func fetchData() {
         let realm = try! Realm()
         
-        if let result = realm.objects(Card.self).last {
+        let results = realm.objects(Card.self)
+        print("results: \(results)")
+        
+        if let result = realm.objects(Card.self).first {
             print("result: \(result)")
             self.card = result
             image = card.image
@@ -45,23 +49,51 @@ class DBEditProfileModel: ObservableObject{
         
     
     func addData() {
-        guard name != "", nickname != "", address != "", image != nil else { return }
+//        guard name != "", nickname != "", address != "", image != nil else { return }
+        
+        let quality = 0.3 // 圧縮率（0.0から1.0の間の値、1.0が最高品質）
+
+        guard let uiImage = UIImage(data: image) else {
+            print("Error converting Data to UIImage")
+            return
+        }
+            
+        guard let jpegData = uiImage.jpegData(compressionQuality: quality) else {
+            print("Error converting UIImage to JPEG Data")
+            return
+        }
             
         do {
             let realm = try Realm()
-            let userCard = realm.objects(Card.self).last!
-            try realm.write {
-                userCard.image = image
-                userCard.name = name
-                userCard.nickname = nickname
-                userCard.address = address
-                userCard.twitter = twitter
-                userCard.instagram = instagram
-                userCard.github = github
-                userCard.blog = blog
-
-                fetchData()
+            if let userCard = realm.objects(Card.self).first {
+                print("userCard: \(userCard)")
+                try realm.write {
+                    userCard.image = jpegData
+                    userCard.name = name
+                    userCard.nickname = nickname
+                    userCard.address = address
+                    userCard.twitter = twitter
+                    userCard.instagram = instagram
+                    userCard.github = github
+                    userCard.blog = blog
+                    print("card updated")
+                }
+            } else {
+                let newCard = Card()
+                newCard.image = jpegData
+                newCard.name = name
+                newCard.nickname = nickname
+                newCard.address = address
+                newCard.twitter = twitter
+                newCard.instagram = instagram
+                newCard.github = github
+                newCard.blog = blog
+                try realm.write {
+                    realm.add(newCard)
+                    print("card created")
+                }
             }
+            fetchData()
         } catch {
             print("Error saving data to Realm: \(error)")
         }
